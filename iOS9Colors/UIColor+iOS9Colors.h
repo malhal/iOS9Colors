f624f1cd0a1cd12424e51bc09590e7e75ef46f36
Malcolm Hall <malcolmhall@users.noreply.github.com>
2016-01-13 20:47:46 +0000

    Initial Commit

---
+++ iOS9Colors/UIColor+iOS9Colors.h
@@ -0,0 +1,43 @@
+//
+//  UIColor+iOS9Colors.h
+//  iOS9Colors
+//
+//  Created by Malcolm Hall on 13/01/2016.
+//  Copyright © 2016 Malcolm Hall. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIColor (iOS9Colors)
+
++(UIColor*)iOS9brown;
+
++(UIColor*)iOS9yellow;
+
++(UIColor*)iOS9orange;
+
++(UIColor*)iOS9blue;
+
++(UIColor*)iOS9green;
+
++(UIColor*)iOS9purple;
+
++(UIColor*)iOS9red;
+
++(UIColor*)iOS9lightBlue;
+
++(UIColor*)iOS9pink;
+
++(UIColor*)iOS9aqua;
+
++(UIColor*)iOS9grey;
+
++(UIColor*)iOS9lightBrown;
+
++(UIColor*)iOS9lightGrey;
+
++(UIColor*)iOS9lightGreen;
+
++(UIColor*)iOS9lightPink;
+
+@end
---
+++ iOS9Colors/UIColor+iOS9Colors.m
@@ -0,0 +1,73 @@
+//
+//  UIColor+iOS9Colors.m
+//  iOS9Colors
+//
+//  Created by Malcolm Hall on 13/01/2016.
+//  Copyright © 2016 Malcolm Hall. All rights reserved.
+//
+
+#import "UIColor+iOS9Colors.h"
+
+@implementation UIColor (iOS9Colors)
+
++(UIColor*)iOS9brown{
+    return [UIColor colorWithRed:204.0f/255.0f green:134.0f/255.0f blue:73.0f/255.0f alpha:1.0f];
+}
+
++(UIColor*)iOS9yellow{
+    return [UIColor colorWithRed:247.0f/255.0f green:179.0f/255.0f blue:20.0f/255.0f alpha:1.0f];
+}
+
++(UIColor*)iOS9orange{
+    return [UIColor colorWithRed:255.0f/255.0f green:141.0f/255.0f blue:54.0f/255.0f alpha:1.0f];
+}
+
++(UIColor*)iOS9blue{
+    return [UIColor colorWithRed:41.0f/255.0f green:162.0f/255.0f blue:255.0f/255.0f alpha:1.0f];
+}
+
++(UIColor*)iOS9green{
+    return [UIColor colorWithRed:31.0f/255.0f green:196.0f/255.0f blue:53.0f/255.0f alpha:1.0f];
+}
+
++(UIColor*)iOS9purple{
+    return [UIColor colorWithRed:191.0f/255.0f green:128.0f/255.0f blue:255.0f/255.0f alpha:1.0f];
+}
+
++(UIColor*)iOS9red{
+    return [UIColor colorWithRed:255.0f/255.0f green:94.0f/255.0f blue:94.0f/255.0f alpha:1.0f];
+}
+
++(UIColor*)iOS9lightBlue{
+    return [UIColor colorWithRed:122.0f/255.0f green:158.0f/255.0f blue:255.0f/255.0f alpha:1.0f];
+}
+
++(UIColor*)iOS9pink{
+    return [UIColor colorWithRed:227.0f/255.0f green:113.0f/255.0f blue:212.0f/255.0f alpha:1.0f];
+}
+
++(UIColor*)iOS9aqua{
+    return [UIColor colorWithRed:10.0f/255.0f green:179.0f/255.0f blue:242.0f/255.0f alpha:1.0f];
+}
+
++(UIColor*)iOS9grey{
+    return [UIColor colorWithRed:134.0f/255.0f green:153.0f/255.0f blue:191.0f/255.0f alpha:1.0f];
+}
+
++(UIColor*)iOS9lightBrown{
+    return [UIColor colorWithRed:210.0f/255.0f green:168.0f/255.0f blue:78.0f/255.0f alpha:1.0f];
+}
+
++(UIColor*)iOS9lightGrey{
+    return [UIColor colorWithRed:166.0f/255.0f green:166.0f/255.0f blue:166.0f/255.0f alpha:1.0f];
+}
+
++(UIColor*)iOS9lightGreen{
+    return [UIColor colorWithRed:120.0f/255.0f green:199.0f/255.0f blue:10.0f/255.0f alpha:1.0f];
+}
+
++(UIColor*)iOS9lightPink{
+    return [UIColor colorWithRed:247.0f/255.0f green:124.0f/255.0f blue:173.0f/255.0f alpha:1.0f];
+}
+
+@end
